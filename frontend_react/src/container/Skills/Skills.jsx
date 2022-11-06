53d6892506303cb33e9f25f7b7e08b6595c69cb1
Ryland Adams <rylandadams@yahoo.com>
2022-11-06 10:16:17 -0800

    skills & experience bones now on to styling

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react'
 import { motion } from 'framer-motion';
-import ReactTolltip from 'react-tooltip'
+import ReactTooltip from 'react-tooltip'
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client'
@@ -18,6 +18,7 @@ const Skills = () => {
     client.fetch(query)
     .then((data) => {
       setExperience(data);
+      console.log(data)
     })
 
     client.fetch(skillsQuery)
@@ -46,6 +47,45 @@ const Skills = () => {
               <p className='p-text'>{skill.name}</p>
             </motion.div>
           ))}
+        </motion.div>
+
+        <motion.div className='app__skills-exp'>
+          {experience?.map((experience) => (
+            <motion.div
+              className='app__skills-exp-item'
+              key={experience.year}
+            >
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{experience.year}</p>
+              </div>
+              <motion.div className='app__skills-exp-works'>  
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                    whileInView={{opacity: [0, 1]}}
+                    transition={{ duration: 0.5}}
+                    className='app__skills-exp-work'
+                    data-tip
+                    data-for={work.name}
+                    key={work.name}
+                    >
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect='solid'
+                      arrowColor='#fff'
+                      className='skills-tooltip'
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+            ))}
+
         </motion.div>
       </div>
     </>
