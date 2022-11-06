74045a3389ec43b11e93f0067033b1b8d33afcc9
Ryland Adams <rylandadams@yahoo.com>
2022-11-06 11:47:34 -0800

    testimonial build

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight} from 'react-icons/hi'
+
+//INTERNAL IMPORTS
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client'
 
 import './Testimonial.scss'
 
 const Testimonial = () => {
+
+  const [brands, setBrands] = useState([])
+  const [testimonials, setTestimonials] = useState([])
+  
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+  
+    client.fetch(query)
+    .then((data) => {
+      setTestimonials(data);
+      console.log(data)
+    })
+
+    client.fetch(brandsQuery)
+    .then((data) => {
+      setBrands(data);
+    })
+  }, [])
+  
   return (
-    <div>Testimonial</div>
+    <>
+      {testimonials.lenth && (
+        <>
+          <div className='app__testimonial-item app__flex'>
+            <img src={urlFor(testimonials[currentIndex].imgurl)} alt='testimonial'/>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg'
+);
--- frontend_react/src/wrapper/MotionWrap.js
+++ frontend_react/src/wrapper/MotionWrap.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-import React from 'react';
-
 const MotionWrap = (Component, classNames) =>
   function HOC() {
     return (
