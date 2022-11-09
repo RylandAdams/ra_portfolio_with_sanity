64aebc738d6a2e8ba082511df59d0ac5bd79b41a
Ryland Adams <rylandadams@yahoo.com>
2022-11-08 16:52:22 -0800

    Fixed work titles on large screens

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className='app__header-circles'
       >
-        {[images.redux, images.react, images.python].map((circle, index) => (
+        {[images.react, images.redux, images.node].map((circle, index) => (
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
